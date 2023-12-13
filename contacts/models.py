2f788a3098704027fa89007cf7dd305b8564ec53
PrithvirajuVenkataraman <prithvirajuvenkataraman@gmail.com>
2023-12-13 02:55:49 -0800

    Last edits!

--- contacts/models.py
+++ contacts/models.py
@@ -5,7 +5,8 @@ class Contact(models.Model):
     name = models.CharField(max_length=255, unique=True)
     email = models.EmailField(unique=True)
     notes = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True, default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return self.name
